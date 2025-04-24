2a95b9fed81a511341e9858a75fdd1af243e32cb
Dyaman99 <dyaman@ualr.edu>
2025-04-24 15:26:57 +0000

    worked on the final

--- final-project/assets/js/app.js
+++ final-project/assets/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         slidesToScroll: 4,
         responsive: [
           {
-            breakpoint: 1024,
+            breakpoint: 1500,
             settings: {
               slidesToShow: 3,
               slidesToScroll: 3,
